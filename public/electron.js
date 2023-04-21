bbf364dfe2e0881288167fbc4348192004dd97cd
ryd3v <hello@ryd3v.com>
2023-04-21 07:22:10 -0400

    Cleanup

--- public/electron.js
+++ public/electron.js
@@ -1,32 +1,31 @@
-// ./public/electron.js
 const path = require('path');
 
-const { app, BrowserWindow, Menu } = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const menu = require('./menu');
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
 }
 
 app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 app.setName('Boilerplate');
 Menu.setApplicationMenu(menu);
